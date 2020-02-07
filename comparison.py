25ffac304f087cc9ec92e7a414d894dea08cffa2
meisterL <pit@DESKTOP-6RHLNHP.localdomain>
2020-02-07 14:50:53 +0100

    comparison function added

---
+++ comparison.py
@@ -0,0 +1,19 @@
+import pubchempy as pcp
+
+def comparison(graph1, graph2):
+    a='h_bond_donor_count'
+    b='h_bond_acceptor_count'
+    c='rotatable_bond_count'
+    d='xlogp'
+
+    args = [a,b,c,d]
+
+    c_1 = pcp.get_compounds(graph1, 'name')
+    p_1 = c_1[0].to_dict(properties=args)
+
+    c_2 = pcp.get_compounds(graph2, 'name')
+    p_2 = c_2[0].to_dict(properties=args)
+
+    euclidean_dist = ( (p_1[d] - p_2[d])**2 + (p_1[a] - p_2[a])**2 + (p_1[b] - p_2[b])**2 + (p_1[c] - p_2[c])**2)**(1/2)
+
+    return euclidean_dist
