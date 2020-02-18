4ce219d3c33922af705bcd0d55cba8f5de6bc79a
jw44lavo <j.wurz@gmx.de>
2020-02-18 16:22:12 +0100

    früher mal gebraucht, jetzt nicht mehr

---
+++ valid_names.py
@@ -0,0 +1,37 @@
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(usage="remove redundant edges from graph file")
+    parser.add_argument("-i", "--input",        metavar="FILE", default=[],     type = str, help= "path to input file")
+    
+    return parser.parse_args()
+
+def main():
+
+    a = get_arguments()
+
+    input_file = a.input
+    results = ""
+    endung = ""
+
+    with open(input_file) as f:
+        for line in f:
+            results += line
+
+    output_file = input_file.split("_")[0]
+    endung = input_file.split(".")[-1]
+
+    output_file = output_file + "." + endung  
+    
+    with open(output_file, "w") as o:
+        o.write(results)
+
+if __name__ == "__main__":
+    
+    try:
+        main()
+
+    except IOError:
+        raise Exception("\n \n ups, something failed tremendously... good luck finding the problem \n")
+
