babb6556cb192b7a520ed66ffda48d668169e522
Praktikumsaccount <praktikum@prak12.bioinf.uni-leipzig.de>
2020-02-20 10:51:23 +0100

    loader with cid output

--- ECtoJSONloader.py
+++ ECtoJSONloader.py
@@ -10,11 +10,37 @@ import shutil
 ################################################################
 ## Strings in list "minis" will be filtered in chemical equation
 ## Add '+' with whitespace at the beginning and the end
+
 def remove_minis(string):
-    minis = ["+ TTP","TTP +","+ UTP","UTP +","+ ITP","ITP +","+ GTP","GTP +","+ CTP","CTP +","+ ADP","ADP +","CoA +","+ CoA","acetate +","+ acetate","+ H2O","H2O +", "+ ATP", "ATP +", "ATP","+ 2 H2O", "CO2", "+ NAD+", "NAD+ +"]
+    minis = ["TTP","UTP","ITP","GTP","CTP","ADP","CoA","acetate","H2O", "ATP", "H2O", "CO2", "NAD+", "NADPH", "H2", "NAD(P)H", "H+", "O2", "NADH", "NADHX", "HCN"]
+    string = string.split("+")
+    new_string =[]
+
+    for s in string:
+        new_string.append(s.strip())
+
+    for s in new_string:
+        if s in minis:
+            new_string.remove(s)
+        if s.isdigit():
+            new_string.remove(s)
+        if s == "more":
+            new_string.remove(s)
+            
+    if len(new_string) > 1:
+        print("ERROR: several substrates, continue...")
+        return None
+    elif not new_string:
+        return None
+    else:
+        return new_string[0]
+"""
+def remove_minis(string):
+    minis = ["+ TTP","TTP +","+ UTP","UTP +","+ ITP","ITP +","+ GTP","GTP +","+ CTP","CTP +","+ ADP","ADP +","CoA +","+ CoA","acetate +","+ acetate","+ H2O","H2O +", "+ ATP", "ATP +", "ATP","+ 2 H2O", "CO2", "+ NAD+", "NAD+ +", "+ H2", "H2 +", "", ""]
     for i in minis:
         string = string.replace(i, "")
     return string
+"""
 
 ################################################################
 ################################################################
@@ -34,12 +60,14 @@ def make_dir(ec, output):
     dir_search="EC"
     for i in s:
         dir_search=f"{dir_search}_{i}"
+    path = os.path.join(os.getcwd(),dir_path, dir_search)
+    print(path)
     try:
-        os.mkdir(f"{dir_path}/{dir_search}")
-        dir_search=f"{dir_path}/{dir_search}"
+        os.makedirs(path)
     except:
         print("mkdir failed")
-    return dir_search
+    
+    return path
 
 def select(list, search):
     selection = []
@@ -62,23 +90,15 @@ def check_for_char(string):
     return True
 
 
-def check_size_of_substrate(name, cutoff):
+def check_size_of_substrate(CID, cutoff):
     counter=0
-    try:
-        p = pcp.get_cids(name, 'name', 'substance', list_return='flat')
-    except:
-        print("substrate not found..")
-        return False
-    if len(p) == 0:
-        print("substrate not found..")
-        return False
-    c = pcp.Compound.from_cid(p[0])
+    c = pcp.Compound.from_cid(CID)
     c = c.to_dict(properties=['atoms', 'bonds', 'inchi'])
     for atom in c['atoms']:
         if atom['element'] is not 'H':
             counter += 1
             if counter > cutoff:
-                print("Strukture of Substrate too big")
+                print("Structure of Substrate too big")
                 return False
     return True
 
@@ -91,9 +111,7 @@ def choose_substrate(proteins):
             for j in i.SP:
                 line = j["data"].split("=")
                 first = remove_minis(line[0])
-                if "more" in str(first):
-                    continue
-                if len(str(first)) == 0:
+                if first == None:
                     continue
                 substrates.append(first)
         except:
@@ -102,7 +120,7 @@ def choose_substrate(proteins):
         counted = Counter(substrates)
         most_common = max(set(substrates), key=substrates.count)
     except:
-        print("mcs doesnt WORK")
+        print("no most common substrate found")
         return None, None
     return most_common, counted
 
@@ -110,26 +128,30 @@ def get_structure(code,cutoff,dir_search,BRENDA_PARSER):
     print("GET STRUCTURE FILES...")
     proteins = BRENDA_PARSER.get_proteins(code)
     substrate, counted = choose_substrate(proteins)
-    #substrate = str(substrate).rstrip()
+
     if substrate == None:
         print("No suitable substrate found, skip..")
         return False
     for s in counted:
         print(str(counted[s]), " : \t", s )
+        
     print("\nmost common: ", substrate)
-    if substrate == "several substrates":
-        print("\nProtein uses several substrates, skip it..")
-        return False
-    if substrate == "more":
-        print("\nProtein uses several substrates, skip it..")
-        return False
-    if check_size_of_substrate(substrate,cutoff) == False:
-        return False
-    #file = (f'{dir_search}/{code}_{str(substrate).strip()}.json')
-    file = (f'{dir_search}/{str(substrate).strip()}.json')
-
+    
     try:
-        pcp.download('JSON', file, substrate, 'name')
+        CID = pcp.get_cids(substrate, 'name', 'substance', list_return='flat')[0]
+    except:
+        print("CID not found..")
+        return False
+    
+    if check_size_of_substrate(CID,cutoff) == False:
+        return False
+
+    file = (f'{dir_search}/{CID}.json')
+    #file = (f'{dir_search}/{str(substrate).strip()}.json')
+    
+    try:
+        pcp.download('JSON', file, CID, 'cid')
+        print("Success, file loaded..")
     except:
         print("substrate not found..")
         return False
@@ -162,6 +184,7 @@ def main():
     dir_search = make_dir(search, output)
     enzyme_list = new_enzyme_list(BRENDA_PARSER)
     enzym_selection = select(enzyme_list, search)
+    
 
     if get_structure_files(enzym_selection,anz,cutoff,dir_search,BRENDA_PARSER):
         print("SUCCESS: Structures found and downloaded")
